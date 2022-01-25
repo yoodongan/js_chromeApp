b0c7029002de02dba8981592f774f1f6faa2f6a5
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-01-25 16:30:33 +0900

    add: weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,25 @@
 const API_KEY = "8dd6c09130c20e35c82805cc1bf895b3";
+const city = document.querySelector(".weather span:first-child");
+const weather = document.querySelector(".weather span:nth-child(2)");
+const temp = document.querySelector(".weather span:nth-child(3)");
+const feel = document.querySelector(".weather span:last-child");
 
-
-function onGeoOk(position){
+function GeoSuccess(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     // lat : 37.5682  lon : 126.9977
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
-    //     .then((response) => response.json())
-    //     .then((data) => {
-            
-
-    //     });
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = `${data.name}`;
+            weather.innerText = `${data.weather[0].main}`;
+            temp.innerText = `${data.main.temp}℃`;
+            feel.innerText = `${data.main.feels_like}℃`;
+        });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
+function GeoFail(){
+    alert("SORRY. CAN'T FIND INFO.");
 
 }
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(GeoSuccess, GeoFail);
